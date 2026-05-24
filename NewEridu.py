977c3eff184a114e5e4249ee40a95870bc810c3d
Lorenzo Gandolfi <lorenzo.gandolfi@mcurie.it>
2026-05-24 15:35:52 +0200

    fogacci2

--- NewEridu.py
+++ NewEridu.py
@@ -12,14 +12,23 @@ pygame.display.set_caption("Mini NukeMap")
 clock = pygame.time.Clock()
 
 # carica mappa
-map_img = pygame.image.load("map.png")
-map_img = pygame.transform.scale(map_img, (WIDTH, HEIGHT))
+original_map = pygame.image.load("map.png").convert()
+
+# zoom
+zoom = 1.0
+min_zoom = 0.5
+max_zoom = 5.0
+
+# offset camera
+offset_x = 0
+offset_y = 0
 
 # lista esplosioni
 explosions = []
 
 class Explosion:
     def __init__(self, x, y, power):
+        # coordinate mondo
         self.x = x
         self.y = y
         self.power = power
@@ -31,12 +40,21 @@ class Explosion:
 
     def draw(self, surface):
 
+        # converte coordinate mondo -> schermo
+        sx = int(self.x * zoom + offset_x)
+        sy = int(self.y * zoom + offset_y)
+
+        # raggi scalati con zoom
+        fireball = int(self.fireball * zoom)
+        shockwave = int(self.shockwave * zoom)
+        thermal = int(self.thermal * zoom)
+
         # termico
         pygame.draw.circle(
             surface,
             (255, 120, 0),
-            (self.x, self.y),
-            int(self.thermal),
+            (sx, sy),
+            thermal,
             2
         )
 
@@ -44,8 +62,8 @@ class Explosion:
         pygame.draw.circle(
             surface,
             (255, 0, 0),
-            (self.x, self.y),
-            int(self.shockwave),
+            (sx, sy),
+            shockwave,
             2
         )
 
@@ -53,8 +71,8 @@ class Explosion:
         pygame.draw.circle(
             surface,
             (255, 255, 0),
-            (self.x, self.y),
-            int(self.fireball)
+            (sx, sy),
+            fireball
         )
 
 running = True
@@ -68,19 +86,60 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # click mouse -> crea esplosione
         if event.type == pygame.MOUSEBUTTONDOWN:
 
-            mx, my = pygame.mouse.get_pos()
+            # zoom con rotella
+            if event.button == 4 or event.button == 5:
 
-            # potenza bomba
-            power = 20
+                mx, my = pygame.mouse.get_pos()
 
-            explosions.append(
-                Explosion(mx, my, power)
-            )
+                # posizione nel mondo PRIMA dello zoom
+                world_x = (mx - offset_x) / zoom
+                world_y = (my - offset_y) / zoom
 
-    screen.blit(map_img, (0, 0))
+                # cambia zoom
+                if event.button == 4:
+                    zoom *= 1.1
+                else:
+                    zoom /= 1.1
 
+                zoom = max(min_zoom, min(max_zoom, zoom))
+
+                # mantiene il cursore fermo sul punto zoomato
+                offset_x = mx - world_x * zoom
+                offset_y = my - world_y * zoom
+
+            # click sinistro
+            elif event.button == 1:
+
+                mx, my = pygame.mouse.get_pos()
+
+                # coordinate schermo -> mondo
+                world_x = (mx - offset_x) / zoom
+                world_y = (my - offset_y) / zoom
+
+                # potenza bomba
+                power = 20
+
+                explosions.append(
+                    Explosion(world_x, world_y, power)
+                )
+
+    # scala mappa
+    map_width = int(original_map.get_width() * zoom)
+    map_height = int(original_map.get_height() * zoom)
+
+    scaled_map = pygame.transform.smoothscale(
+        original_map,
+        (map_width, map_height)
+    )
+
+    # disegna mappa
+    screen.fill((0, 0, 0))
+    screen.blit(scaled_map, (offset_x, offset_y))
+
+    # disegna esplosioni
     for exp in explosions:
         exp.draw(screen)
 
